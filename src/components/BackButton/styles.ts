1689812d1179ab0c4bae2641d6637b8bd3a519b7
elias alexandre <elliasallex@gmail.com>
2021-12-24 22:02:58 -0300

    list Car schedules by user

--- src/components/BackButton/styles.ts
+++ src/components/BackButton/styles.ts
@@ -2,5 +2,6 @@ import { BorderlessButton } from "react-native-gesture-handler";
 import styled from "styled-components/native";
 
 export const Container = styled(BorderlessButton)`
-  flex: 1;
+  max-width: 80px;
+  width: 100%;
 `;
--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,20 +1,58 @@
 import React, { useEffect, useState } from "react";
-import { Alert } from "react-native";
-import { Load } from "../../components/Load";
+import { StatusBar } from "expo-status-bar";
+import { Alert, FlatList } from "react-native";
+import { useTheme } from "styled-components";
 
+import { AntDesign } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+
+import { BackButton } from "../../components/BackButton";
+import { Load } from "../../components/Load";
+import { Car } from "../../components/Car";
 import { CarDTO } from "../../dtos/CarDTO";
 import { api } from "../../services/api";
 
-import { Container } from "./styles";
+import {
+  Container,
+  Header,
+  Title,
+  Subtitle,
+  Content,
+  Appointments,
+  AppointmentTitle,
+  AppointmentCount,
+  CarWrapper,
+  CarFooter,
+  CarFooterTitle,
+  CarFooterDate,
+  CarFooterPeriod,
+} from "./styles";
+
+interface CarProps {
+  id: string;
+  user_id: string;
+  startDate: string;
+  endDate: string;
+  car: CarDTO;
+}
 
 export function MyCars() {
-  const [cars, setCars] = useState<CarDTO[]>([]);
+  const [cars, setCars] = useState<CarProps[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const theme = useTheme();
+  const navigation = useNavigation<any>();
+
+  function handleBack() {
+    navigation.goBack();
+  }
 
   useEffect(() => {
     async function fetchCars() {
       try {
-        const { data } = await api.get<CarDTO[]>(`/schedules_byuser?user_id=1`);
+        const { data } = await api.get<CarProps[]>(
+          `/schedules_byuser?user_id=1`
+        );
         setCars(data);
       } catch (error: any) {
         Alert.alert(error.message);
@@ -26,5 +64,57 @@ export function MyCars() {
     fetchCars();
   }, []);
 
-  return <Container>{isLoading && <Load />}</Container>;
+  return (
+    <Container>
+      <StatusBar style="light" />
+
+      <Header>
+        <BackButton onPress={handleBack} color={theme.colors.shape} />
+
+        <Title>
+          Seus agendamentos,{"\n"}
+          estão aqui.
+        </Title>
+
+        <Subtitle>Conforto, segurança e praticidade.</Subtitle>
+      </Header>
+
+      {isLoading ? (
+        <Load />
+      ) : (
+        <Content>
+          <Appointments>
+            <AppointmentTitle>Agendamentos feitos</AppointmentTitle>
+            <AppointmentCount>24</AppointmentCount>
+          </Appointments>
+
+          <FlatList
+            showsVerticalScrollIndicator={false}
+            data={cars}
+            keyExtractor={(item) => String(item.id)}
+            renderItem={({ item }) => (
+              <CarWrapper>
+                <Car data={item.car} />
+
+                <CarFooter>
+                  <CarFooterTitle>Periodo</CarFooterTitle>
+
+                  <CarFooterPeriod>
+                    <CarFooterDate>{item.startDate}</CarFooterDate>
+                    <AntDesign
+                      name="arrowright"
+                      size={20}
+                      color={theme.colors.title}
+                      style={{ marginHorizontal: 10 }}
+                    />
+                    <CarFooterDate>{item.endDate}</CarFooterDate>
+                  </CarFooterPeriod>
+                </CarFooter>
+              </CarWrapper>
+            )}
+          />
+        </Content>
+      )}
+    </Container>
+  );
 }
